59d09dc73461b6f3015f1a8addbc33a48b95a47f
Kameron Brooks <kameron.cw.11@gmail.com>
2024-11-15 11:06:45 -0500

    Created TrackIndexingResult class

--- app/src/main/java/TrackIndexer.java
+++ app/src/main/java/TrackIndexer.java
@@ -1,2 +1,16 @@
 public class TrackIndexer {
+    public TrackIndexingResult indexFileSystem() {
+        // TODO
+        return null;
+    }
+
+    public TrackIndexingResult indexStreamingServices() {
+        // TODO
+        return null;
+    }
+
+    public TrackIndexingResult indexStreamingService() {
+        // TODO
+        return null;
+    }
 }
---
+++ app/src/main/java/TrackInfo.java
@@ -0,0 +1,17 @@
+import android.media.Image;
+
+public class TrackInfo {
+    public int requestId;
+    public int mediaId;
+    public String mediaLocator;
+    public String mediaSourceType;
+    public String trackName;
+    public float trackLengthSeconds;
+    // public Artist artist;
+    // public Album album;
+    public String genre;
+    public int trackNumber;
+    public Image image;
+    public String lyrics;
+    public float[] embedding;
+}
