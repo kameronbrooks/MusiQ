0259c41af54928c3d4e7d5fa898086ae7bf965d4
Kameron brooks <kameron.cw.11@gmail.com>
2024-11-28 22:37:04 -0500

    Started incorperating the UI into the project

---
+++ app/src/main/java/com/g2/musiq/MainActivity.java
@@ -0,0 +1,24 @@
+package com.g2.musiq;
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.g2.musiq.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/g2/musiq/PlayerActivity.java
@@ -0,0 +1,50 @@
+package com.g2.musiq;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class PlayerActivity extends AppCompatActivity {
+    TextView txt_song_name;
+    SeekBar songSeekBar;
+    Button btn_play_pause, btn_next, btn_prev;
+
+    int position;
+    String song;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        try {
+            getSupportActionBar().setTitle("Now Playing");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        txt_song_name = findViewById(R.id.txt_song_name);
+        songSeekBar = findViewById(R.id.songSeekbar);
+        btn_play_pause = findViewById(R.id.btn_play_pause);
+        btn_next = findViewById(R.id.btn_next);
+        btn_prev = findViewById(R.id.btn_prev);
+
+
+    }
+}
